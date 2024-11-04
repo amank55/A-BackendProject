c44739db3b3a6199345cb5fe584ef1fad49e350b
amank55 <amankumarr2005ak@gmail.com>
2024-11-04 20:58:20 +0530

    Introduced Postman

--- src/index.js
+++ src/index.js
@@ -2,27 +2,20 @@
 import dotenv from "dotenv"
 
 import connectDB from "./db/index.js"
-
+import {app} from './app.js'
 dotenv.config({
     path: './.env'
 })
 
 connectDB()
-
-.then(()=>{
-    (process.env.PORT || 8000, ()=>{
-        console.log(`Server is running at succesfully :${process.env.PORT}`);
+.then(() => {
+    app.listen(process.env.PORT || 8002, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
-.catch((err)=>{
-    console.log("Mongodb Connection failed !!!!:", err);
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
-
-
-
-
- 
-
 
 
 
